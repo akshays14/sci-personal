32da9572caaa803f027016d0d6f2e8940b9932cd
akshays14 <akshay@sciencescape.net>
2015-03-27 19:10:55 -0400

    Adding version 2 of de-normalizing project.

---
+++ UnnormalizeCoreDBv2/src/main/java/com/sciencescape/ds/data/transfer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * com.sciencescape.ds.data.transfer is group of classes
+ * to de-normalize and transfer data from RDBMS to HBase.
+ *
+ * @author akshay
+ */
+package com.sciencescape.ds.data.transfer;
